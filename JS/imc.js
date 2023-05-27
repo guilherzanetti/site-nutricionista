f7c62d7a2af9dfedfe57704622d535ae1c9929c3
Guilherme Zanetti <94937630+guilherzanetti@users.noreply.github.com>
2023-05-27 18:04:42 -0300

    imc-responsive-version

--- JS/imc.js
+++ JS/imc.js
@@ -1,32 +1,3 @@
-function calcularIMC() {
-    var alturaStr = document.getElementById("alturaInput").value;
-    var pesoStr = document.getElementById("pesoInput").value;
-  
-    // Substituir vírgulas por pontos
-    var altura = parseFloat(alturaStr.replace(",", "."));
-    var peso = parseFloat(pesoStr.replace(",", "."));
-  
-    if (isNaN(altura) || isNaN(peso)) {
-      alert("Por favor, insira valores numéricos válidos.");
-      return;
-    }
-  
-    var imc = peso / (altura * altura);
-    var resultado = "";
-  
-    if (imc < 18.5) {
-      resultado = "Abaixo do peso";
-    } else if (imc >= 18.5 && imc < 25) {
-      resultado = "Peso saudável";
-    } else if (imc >= 25 && imc < 30) {
-      resultado = "Sobrepeso";
-    } else {
-      resultado = "Obesidade";
-    }
-  
-    document.getElementById("resultadoIMC").innerHTML = "Seu IMC é: " + imc.toFixed(2) + "<br>" + "Classificação: " + resultado;
-    document.getElementById("resultadoIMC").style.display = "block";
-  }
 
   //-- FUNCIONALIDADE IMC
 
@@ -60,3 +31,13 @@ function calcularIMC() {
     document.getElementById("resultadoIMC").style.display = "block";
   }
   
+  // Header responsivo
+
+  //HEADER RESPONSIVO
+
+const btnHeaderResponsive = document.querySelector('.btn-header-responsive');
+const responsiveOptions = document.querySelector('.responsive-options');
+
+btnHeaderResponsive.addEventListener('click', () => {
+    responsiveOptions.classList.toggle('show');
+});
